8a868fdf6a781f51f4f6efbd55945f16493650b5
chengmingye@gmail.com <chengmingye@gmail.com@d40b962f-f7b7-1e2a-71cc-4cdb7cb8c414>
2011-06-02 05:58:31 +0000

    copyright information added. (java files)

--- trunk/Monopoly/src/com/iiiss/spring/hibernate/HibernatePM.java
+++ trunk/Monopoly/src/com/iiiss/spring/hibernate/HibernatePM.java
@@ -10,6 +10,27 @@ import org.springframework.transaction.annotation.Transactional;
 import com.iiiss.spring.common.IBean;
 import com.iiiss.spring.common.IPersistenceManager;
 
+/*
+ * Monopoly - com.iiiss.spring.hibernate.HibernatePM.java
+ * 
+ * Copyright (C) 2011  Mingye Cheng <Cheng, Mingye at GMAIL>
+ * 
+ * HibernatePM.java is part of <iiiStudio Template Framework> project.
+ * This project is hosted at <http://iiiss-template-ssh.googlecode.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 @Transactional
 public class HibernatePM extends HibernateDaoSupport implements
 		IPersistenceManager {
--- trunk/Monopoly/src/com/iiiss/spring/impl/test/UserControllerTest.java
+++ trunk/Monopoly/src/com/iiiss/spring/impl/test/UserControllerTest.java
@@ -12,6 +12,27 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
+/*
+ * Monopoly - com.iiiss.spring.impl.test.UserControllerTest.java
+ * 
+ * Copyright (C) 2011  Mingye Cheng <Cheng, Mingye at GMAIL>
+ * 
+ * UserControllerTest.java is part of <iiiStudio Template Framework> project.
+ * This project is hosted at <http://iiiss-template-ssh.googlecode.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class UserControllerTest extends BaseControllerTest {
 	@Autowired
 	private IUserDao userDao;
