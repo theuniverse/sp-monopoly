4567e1fbb61b7c65bd2977c8c346ffbbb26c7783
jiruoan@gmail.com <jiruoan@gmail.com@d40b962f-f7b7-1e2a-71cc-4cdb7cb8c414>
2011-06-21 08:23:07 +0000

    Two warnings fixed

--- trunk/Monopoly/src/monopoly/hibernate/beans/event/Event.java
+++ trunk/Monopoly/src/monopoly/hibernate/beans/event/Event.java
@@ -1,18 +1,14 @@
 package monopoly.hibernate.beans.event;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.ManyToOne;
 
 import com.iiiss.spring.common.BaseBean;
 
-import monopoly.core.beans.IEventQueue;
 import monopoly.core.beans.event.IEvent;
-import monopoly.hibernate.beans.EventQueue;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
